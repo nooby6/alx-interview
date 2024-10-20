f5cf9e5e19fbb082a3ee52e8c3ccf46a7b7ca455
nooby6 <giftaudrine1@gmial.com>
2024-10-20 12:27:16 -0700

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function to calculate the minimum number of operations
+to achieve exactly n 'H' characters in the file.
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
