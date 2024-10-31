dd304865838980bef0640fbaf236621c75402bd8
nooby6 <giftaudrine1@gmail.com>
2024-10-31 19:38:21 +0300

    Refactor UTF-8 validation function for clarity and efficiency

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
-"""
-Function to validate if a given data set is valid UTF-8 encoding.
-"""
+"""Contains a method for validating data as UTF-8"""
 
-def validUTF8(data):
-    num_bytes = 0
+from typing import List
 
-    # Masks to check the most significant bits
-    mask1 = 1 << 7      # 10000000
-    mask2 = 1 << 6      # 01000000
 
-    for byte in data:
-        # Mask to keep only the 8 least significant bits
-        byte &= 0xFF
+def invalid_byte(val: int) -> bool:
+    """Checks whether an integer can be represented in 7bits"""
+    return abs(val) >= 2**7
 
-        if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if (byte & mask1) == 0:
-                continue  # 1-byte character (ASCII)
-            elif (byte & (mask1 >> 1)) == mask1:
-                num_bytes = 1  # 2-byte character
-            elif (byte & (mask1 >> 2)) == (mask1 | mask2):
-                num_bytes = 2  # 3-byte character
-            elif (byte & (mask1 >> 3)) == (mask1 | mask2 | (mask1 >> 2)):
-                num_bytes = 3  # 4-byte character
-            else:
-                return False
-        else:
-            # Check if byte is a continuation byte (10xxxxxx)
-            if (byte & mask1) != mask1 or (byte & mask2) == 0:
-                return False
-            num_bytes -= 1
 
-    return num_bytes == 0
+def validUTF8(data: List[int]) -> bool:
+    """Determines of a given data set represents a valid UTF-8 encoding
 
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    return not any((invalid_byte(val) for val in data))
